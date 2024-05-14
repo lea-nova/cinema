8919c2d5905f0da82794586b0fc7f08eb28b1a3e
Jordan SENA <jordan@MacBook-Air-de-Jordan.local>
2024-05-14 15:41:21 +0200

    k

--- model/repository/UserDao.php
+++ model/repository/UserDao.php
@@ -13,7 +13,7 @@ class UserDao extends Dao
     //Récupérer toutes les items
     public static  function getAll(): array
     {
-        $query = self::$bdd->prepare(" SELECT * FROM acteur");
+        $query = self::$bdd->prepare(" SELECT * FROM utilisateur");
         $query->execute();
         $user = array();
         while ($data = $query->fetch()) {
@@ -31,7 +31,7 @@ class UserDao extends Dao
     //Récupérer plus d'info sur 1 item à l'aide de son id
     public static function getOne(int $id): Object
     {
-        $query = self::$bdd->prepare('SELECT * from acteur WHERE id = :id_user');
+        $query = self::$bdd->prepare('SELECT * from utilisateur WHERE id = :id_user');
         $query->execute(array(':id_user' => $id));
         $data = $query->fetch();
         return new User($data['id'], $data['username'], $data['email'], $data['password']);
@@ -40,7 +40,7 @@ class UserDao extends Dao
     //Ajouter un item
     public static function addOne(Object $data): bool
     {
-        $query = self::$bdd->prepare('INSERT INTO acteur VALUES (:id, :username, :email, :password)');
+        $query = self::$bdd->prepare('INSERT INTO utilisateur VALUES (:id, :username, :email, :password)');
         $value = ['id' => $data->getIdUser(), 'username' => $data->getUsername(), 'email' => $data->getEmail(), 'password' => $data->getPrenom()];
         $insert = self::$bdd->prepare($query);
         return $insert->execute($value);
