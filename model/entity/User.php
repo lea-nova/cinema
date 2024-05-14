cf54237250ffe1ac3f697fe0bbf20ffe710bb20a
Jordan SENA <jordan@MacBook-Air-de-Jordan.local>
2024-05-14 09:35:54 +0200

    User.php

---
+++ model/entity/User.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace Model\entity;
+
+class User
+{
+    private $username;
+    private $email;
+    private $password;
+
+    public function __construct(string $username = "", string $email = "", string  $password = "")
+    {
+        $this->setUsername($username);
+        $this->setEmail($email);
+        $this->setPassword($password);
+    }
+
+    /**
+     * Get the value of username
+     */
+    public function getUsername()
+    {
+        return $this->username;
+    }
+
+    /**
+     * Set the value of username
+     *
+     * @return  self
+     */
+    public function setUsername($username)
+    {
+        $this->username = $username;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of email
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * Set the value of email
+     *
+     * @return  self
+     */
+    public function setEmail($email)
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of password
+     */
+    public function getPassword()
+    {
+        return $this->password;
+    }
+
+    /**
+     * Set the value of password
+     *
+     * @return  self
+     */
+    public function setPassword($password)
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+}
