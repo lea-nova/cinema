858dab8d6fe95d882db3fce6c7c6b9e2752c8d8c
Jordan SENA <jordan@MacBook-Air-de-Jordan.local>
2024-05-16 19:43:03 +0200

    modif

--- controller/home.php
+++ controller/home.php
@@ -1,14 +1,16 @@
 <?php
 
-//On affiche le template Twig correspondant
-
+// use Model\entity\Acteur;
+// use Model\repository\ActeurDao;
+use Model\entity\Film;
+use Model\entity\Role;
+use Model\entity\Acteur;
 use Model\repository\FilmDao;
-use Model\repository\UserDao;
 
-$userDao = new UserDao();
-$user = $userDao::getAll();
 
-// $filmDao = new FilmDao();
-// $films = $filmDao::getAll();
+$role = new FilmDao();
+$films = $role->getAll();
 
-echo $twig->render('home.html.twig');
+echo $twig->render('home.html.twig', [
+    "films" => $films
+]);
--- model/repository/FilmDao.php
+++ model/repository/FilmDao.php
@@ -8,12 +8,11 @@ use Model\entity\Acteur;
 use Model\repository\Dao;
 
 
-
 class FilmDao extends Dao
 {
     public static function getAll(): array
     {
-        $query = self::$bdd->prepare("SELECT * FROM film JOIN role ON role.id_Film=film.id JOIN acteur ON acteur.id = role.id_Acteur GROUP BY film.id");
+        $query = self::$bdd->prepare("SELECT * FROM film JOIN role ON role.id_Film=film.id JOIN acteur ON acteur.id = role.id_Acteur");
         $query->execute();
         $films = array();
         $filmDao = new FilmDao();
@@ -47,9 +46,15 @@ class FilmDao extends Dao
         return  $films[] = new Film($data['id'], $data['titre'], $data['realisateur'], $data['affiche'], $data['annee'], $roles[$role]);
     }
 
-    public function addRole()
+    public function addRole($idFilm, $idRole, $personnage)
     {
+        $query = self::$bdd->prepare("INSERT INTO role (id_film, id_acteur, personnage) VALUES (:film_id, :role_id, :personnage)");
+        $query->bindParam(':film_id', $idFilm, \PDO::PARAM_INT);
+        $query->bindParam(':role_id', $idRole, \PDO::PARAM_INT);
+        $query->bindParam(':personnage', $personnage, \PDO::PARAM_STR);
+        return $query->execute();
     }
+
     public function getRole($id): array
     {
         $roles = array();
