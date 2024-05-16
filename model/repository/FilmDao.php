8744e7418959c4a42eac3aae043d36f34f7e1b89
Jordan SENA <jordan@MacBook-Air-de-Jordan.local>
2024-05-16 14:06:21 +0200

    ajout du fichier FilmDao

--- model/repository/FilmDao.php
+++ model/repository/FilmDao.php
@@ -13,13 +13,19 @@ class FilmDao extends Dao
 {
     public static function getAll(): array
     {
-
-        $query = self::$bdd->prepare("SELECT film.id, film.titre, film.realisateur, film.affiche, film.annee, role.id, role.personnage, acteur.id, acteur.nom, acteur.prenom FROM film JOIN role ON role.id_Film = film.id JOIN acteur ON role.id_Acteur = acteur.id");
+        $query = self::$bdd->prepare("SELECT * FROM film JOIN role ON role.id_Film=film.id JOIN acteur ON acteur.id = role.id_Acteur GROUP BY film.id");
+        $query = self::$bdd->prepare("SELECT * FROM film JOIN role ON role.id_Film=film.id JOIN acteur ON acteur.id = role.id_Acteur GROUP BY film.id");
         $query->execute();
         $films = array();
-
+        $filmDao = new FilmDao();
+        $filmDao = new FilmDao();
         while ($data = $query->fetch()) {
-            $films[] = new Film($data['id'], $data['titre'], $data['realisateur'], $data['affiche'], $data['annee'], $data['role']);
+            $roles = array();
+            $roles = $filmDao->getRole($data['id_Film']);
+            $films[] = new Film($data['id'], $data['titre'], $data['realisateur'], $data['affiche'], $data['annee'], $roles);
+            $roles = array();
+            $roles = $filmDao->getRole($data['id_Film']);
+            $films[] = new Film($data['id'], $data['titre'], $data['realisateur'], $data['affiche'], $data['annee'], $roles);
         }
         return ($films);
     }
@@ -37,23 +43,48 @@ class FilmDao extends Dao
     //Récupérer plus d'info sur 1 film
     public static function getOne(int $id): Film
     {
-        $query = self::$bdd->prepare('SELECT * FROM film WHERE id = :id_film');
-        $query->execute(array(':id_film' => $id));
+        $query = self::$bdd->prepare('SELECT 
+        film.id, film.titre, film.realisateur, film.affiche, film.annee, 
+        role.id, role.personnage, 
+        acteur.id, acteur.nom, acteur.prenom 
+                FROM film 
+                JOIN role ON role.id_Film = film.id 
+                JOIN acteur ON role.id_Acteur = acteur.id
+                WHERE film.id = :id');
+        $query->execute(array(':id' => $id));
         $data = $query->fetch();
-        $roles = new Role($data['id'], $data['personnage'], new Acteur($data['id'], $data['nom'], $data['prenom']));
-        return new Film($data['id'], $data['titre'], $data['realisateur'], $data['affiche'], $data['annee'], $data['role']);
+        $roles = array();
+        while ($row = $query->fetch()) {
+            $acteur = new Acteur($data['id'], $data['nom'], $data['prenom']);
+            $role = new Role($data['id'], $data['personnage'], $acteur);
+            $roles[] = $role;
+        }
+
+        return new Film($data['id'], $data['titre'], $data['realisateur'], $data['affiche'], $data['annee'], $roles);
     }
 
-    public static function search($query)
+    public function addRole($idFilm, $idRole, $personnage)
     {
-        $query = self::$bdd->prepare('SELECT * FROM film WHERE titre LIKE :query');
-        $searchQuery = '%' . $query . '%';
-        $query->bindParam(':query', $searchQuery);
-        $query->execute();
-        $results = [];
+        $query = self::$bdd->prepare("INSERT INTO role (id_film, id_acteur, personnage) VALUES (:film_id, :role_id, :personnage)");
+        $query->bindParam(':film_id', $idFilm, \PDO::PARAM_INT);
+        $query->bindParam(':role_id', $idRole, \PDO::PARAM_INT);
+        $query->bindParam(':personnage', $personnage, \PDO::PARAM_STR);
+        return $query->execute();
+    }
+    public function getRole($id): array
+    {
+        $roles = array();
+        $query = self::$bdd->prepare("SELECT *  FROM `role` JOIN film ON film.id = role.id_Film JOIN acteur on role.id_Film = :id WHERE role.id_Acteur= acteur.id ");
+        $query->execute(array(':id' => $id));
         while ($data = $query->fetch()) {
-            $results[] = new Film($data['id'], $data['titre'], $data['realisateur'], $data['affiche'], $data['annee'], $data['roles']);
+            $acteur = new Acteur($data['id'], $data['nom'], $data['prenom']);
+            $role = new Role($data['id'], $data['personnage'], $acteur);
+            $roles[] = $role;
         }
-        return $results;
+        return $roles;
+        $roles = array();
+        $role = new Role($data['id'], $data['personnage'], new Acteur($data['id'], $data['nom'], $data['prenom']));
+
+        return  $films[] = new Film($data['id'], $data['titre'], $data['realisateur'], $data['affiche'], $data['annee'], $roles[$role]);
     }
 }
